923a33640d1018a2a84059d8cc8607d6555d11ce
GuilhermeBeltrao <guilhermecezar.beltrao@gmail.com>
2021-02-22 06:00:17 -0300

    new file:   VisitaNaFeira/Program.cs

---
+++ VisitaNaFeira/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VisitaNaFeira
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string entry = Console.ReadLine();
+            var numbers = entry.Split(" ");
+
+            int a = int.Parse(numbers[0]);
+            int b = int.Parse(numbers[1]);            
+            int x = a + b;
+
+            Console.WriteLine($"X = {x}");
+        }
+    }
+}
