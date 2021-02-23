af600e8f4dd316faad979746cf8c6ae1b9e9f685
GuilhermeBeltrao <guilhermecezar.beltrao@gmail.com>
2021-02-22 23:51:24 -0300

    new file:   Tuitando/Program.cs

---
+++ Tuitando/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tuitando
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string v = Console.ReadLine();
+            char[] arr = v.ToCharArray();
+            if( arr.Length <= 140  )
+            { 
+                Console.WriteLine("TWEET");
+            }
+            else
+            {
+                Console.WriteLine("MUTE");
+            }
+        }
+    }
+}
